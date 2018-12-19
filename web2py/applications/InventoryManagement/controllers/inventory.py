4ee70d31fc5c8a0b6f1615338945d4afdc7d2aab
daiarn <dainius1997@gmail.com>
2018-12-19 13:27:03 +0200

    Added functions to render inventory files

---
+++ web2py/applications/InventoryManagement/controllers/inventory.py
@@ -0,0 +1,6 @@
+def inventory():
+    response.flash = T("Hello World")
+    return dict(message=T('Welcome to web2py!'))
+def inventtoryInUse():
+    response.flash = T("Hello World")
+    return dict(message=T('Welcome to web2py!'))
