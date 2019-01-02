dbac6379a012851b4b938ceb80709d07e5f4deba
daiarn <dainius1997@gmail.com>
2019-01-02 02:44:38 +0200

    login and register functions now returns forms

--- web2py/applications/InventoryManagement/controllers/user.py
+++ web2py/applications/InventoryManagement/controllers/user.py
@@ -1,7 +1,13 @@
 # ---- example login page ----
 def login():
-    response.flash = T("Hello World")
-    return dict(message=T('Welcome to web2py!'))
+    # response.flash = T("Hello World")
+    # return dict(message=T('Welcome to web2py!'))
+    # auth.logout()
+    return dict(form=auth.login())
+
+
 def register():
-    response.flash = T("Hello World")
-    return dict(message=T('Welcome to web2py!'))
+    # response.flash = T("Hello World")
+    # return dict(message=T('Welcome to web2py!'))
+    # auth.logout()
+    return dict(form=auth.register())
