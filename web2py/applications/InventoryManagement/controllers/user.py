74702ef019c9f9627b0bce48413c9e7a3bb8bddb
daiarn <dainius1997@gmail.com>
2019-01-04 01:44:22 +0200

    added function to register company

--- web2py/applications/InventoryManagement/controllers/user.py
+++ web2py/applications/InventoryManagement/controllers/user.py
@@ -2,7 +2,7 @@
 def login():
     # response.flash = T("Hello World")
     # return dict(message=T('Welcome to web2py!'))
-    # auth.logout()
+    auth.logout()
     return dict(form=auth.login())
 
 
@@ -11,3 +11,9 @@ def register():
     # return dict(message=T('Welcome to web2py!'))
     # auth.logout()
     return dict(form=auth.register())
+
+
+def company_registration():
+    user = auth.user
+    form = SQLFORM(db.company).process()
+    return dict(form=form, user=user)
