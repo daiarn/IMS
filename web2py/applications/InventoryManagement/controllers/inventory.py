4ada039aa4ce634dbdb1520f2cc1effcf09979b9
daiarn <dainius1997@gmail.com>
2019-01-03 12:45:22 +0200

    all controllers return user email

--- web2py/applications/InventoryManagement/controllers/inventory.py
+++ web2py/applications/InventoryManagement/controllers/inventory.py
@@ -1,18 +1,22 @@
 def inventory():
+    user_email = auth.user.email
     response.flash = T("Hello World")
-    return dict(message=T('Welcome to web2py!'))
+    return dict(message=T('Welcome to web2py!'), user_email=user_email)
 
 
 def inventoryInUse():
+    user_email = auth.user.email
     response.flash = T("Hello World")
-    return dict(message=T('Welcome to web2py!'))
+    return dict(message=T('Welcome to web2py!'), user_email=user_email)
 
 
 def addItem():
+    user_email = auth.user.email
     response.flash = T("Hello World")
-    return dict(message=T('Welcome to web2py!'))
+    return dict(message=T('Welcome to web2py!'), user_email=user_email)
 
 
 def removeItem():
+    user_email = auth.user.email
     response.flash = T("Hello World")
-    return dict(message=T('Welcome to web2py!'))
+    return dict(message=T('Welcome to web2py!'), user_email=user_email)
--- web2py/applications/InventoryManagement/controllers/report.py
+++ web2py/applications/InventoryManagement/controllers/report.py
@@ -1,3 +1,4 @@
 def report():
+    user_email = auth.user.email
     response.flash = T("Hello World")
-    return dict(message=T('Welcome to web2py!'))
+    return dict(message=T('Welcome to web2py!'), user_email=user_email)
