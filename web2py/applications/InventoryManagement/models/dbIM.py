e69db4c830ccfa0b7fb9c5e6ad40462cdfd12679
daiarn <dainius1997@gmail.com>
2019-01-12 22:58:21 +0200

    Removed useless table user_admin

--- web2py/applications/InventoryManagement/models/dbIM.py
+++ web2py/applications/InventoryManagement/models/dbIM.py
@@ -1,10 +1,1 @@
 # -*- coding: utf-8 -*-
-db.define_table(
-    'user_admin',
-    Field('name', requires=IS_NOT_EMPTY()),
-    Field('surname', requires=IS_NOT_EMPTY()),
-    Field('email', requires=IS_NOT_EMPTY()),
-    Field('password', 'password', requires=IS_NOT_EMPTY()),
-    Field('role'),
-    Field('time_stamp', 'datetime')
-    )
