f7281d46995b8e8ca8ddad2b0aaf30642882e3ed
daiarn <dainius1997@gmail.com>
2019-01-15 19:47:48 +0200

    Fixed. Now memebers can not see what is only for admin

--- web2py/applications/InventoryManagement/controllers/inventory.py
+++ web2py/applications/InventoryManagement/controllers/inventory.py
@@ -1,6 +1,7 @@
 def inventory():
     user = auth.user
     comp = get_user_company(user)
+    membership = db(db.auth_membership.user_id == user.id).select().first()
     fields = (db.item.id, db.item.Name, db.item.Worth, db.item.Category, db.item.Status)
     headers = {'item.id': 'ID',
                'item.Name': 'Name',
@@ -8,36 +9,44 @@ def inventory():
                'item.Category': 'Category',
                'item.Status': 'Status'}
     default_sort_order = [db.item.Name]
-    form = SQLFORM.grid(db.item.company_id == comp.id, fields=fields, headers=headers, orderby=default_sort_order, maxtextlength=64, paginate=25)
-    return dict(user=user, company=comp, form=form)
+    form = SQLFORM.grid(db.item.company_id == comp.id, create=False, fields=fields, headers=headers,
+                        orderby=default_sort_order, maxtextlength=64, paginate=25,
+                        editable=True, deletable=auth.has_membership('ROLE_ADMIN'))
+    return dict(user=user, company=comp, form=form, membership=membership)
 
 
 def inventoryInUse():
     user = auth.user
     comp = get_user_company(user)
+    membership = db(db.auth_membership.user_id == user.id).select().first()
     rows = db((db.item.Status == 'Taken')&(db.item.company_id == comp.id)).select()
-    return dict(user=user, company=comp, rows=rows)
+    return dict(user=user, company=comp, rows=rows, membership=membership)
 
 
 def addItem():
     form=SQLFORM(db.item)
     user = auth.user
     comp = get_user_company(user)
+    membership = db(db.auth_membership.user_id == user.id).select().first()
     if form.process().accepted:
         new_item = db(db.item).select().last()
         db(db.item.id == new_item.id).update(company_id=comp.id)
-    return dict(form=form, user=user, company=comp)
+        if new_item.Status == 'Taken':
+            db(db.item.id == new_item.id).update(Taken=1)
+    return dict(form=form, user=user, company=comp, membership=membership)
 
 
 def removeItem():
     user = auth.user
     comp = get_user_company(user)
+
     return dict(user=user, company=comp)
 
 
 def add_category():
     user = auth.user
     company = get_user_company(user)
+    membership = db(db.auth_membership.user_id == user.id).select().first()
     form = SQLFORM(db.category).process()
     return locals()
 
@@ -45,8 +54,13 @@ def add_category():
 def edit_item():
     user = auth.user
     company = get_user_company(user)
+    membership = db(db.auth_membership.user_id == user.id).select().first()
     item = db.item(request.args(0, cast=int))
-    form = SQLFORM(db.item, item).process()
+    form = SQLFORM(db.item, item)
+    if form.process().accepted:
+        if item.Status == 'Taken':
+            new_count = int(float(item.Taken)) + 1
+            db(db.item.id == item.id).update(Taken=new_count)
     return locals()
 
 
